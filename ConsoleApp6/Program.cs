91a9be93c2ed674ef3bb634fbbf8a3b9f7b5a69f
Admin <Admin@TEB127001>
2023-05-12 09:25:56 +0200

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Metod_inl_2
+{
+    class Program
+    {
+        static int största(int tal1, int tal2)
+        {
+            if (tal1 < tal2)
+            {
+                return tal2;
+            }
+
+            else if (tal1 > tal2)
+            {
+                return tal1;
+            }
+
+            else
+            {
+                return (tal1 + tal2)    /2;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in 2 heltal");
+            Console.WriteLine("Tal 1");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Tal 2");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int summa = största(x, y);
+            Console.WriteLine("Det största talet är " + summa);
+        }
+    }
+}
+
+
